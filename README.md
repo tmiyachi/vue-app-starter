f1ddf2b8899850da56a33be534d9de1875c992a3
tmiyachi <t.miyachi@hotmail.co.jp>
2020-08-30 01:13:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# js-app-starter
+
+JavsScript を用いた Vue.js アプリ開発のための個人用スターター
+
+## Description
+
+- webpack によるバンドル
+- ESlint，stylelint による静的コード解析
+- Prettier によるフォーマット
+- Babel によるトランスコンパイル
+
+詳細は https://www.notion.so/JS-Web-8964358b29714a68898c13399e98435e
+
+## Note
+Vue3準拠にするため [eslint-plugin-vue](https://github.com/vuejs/eslint-plugin-vue) はバージョン7.xを使用している．
+2020/8/30現在ではリリース前のため `npm install eslint-plugin-vue@next` でリリース前バージョンを指定している．
+
+## Usage
+
+Install package
+
+```
+npm install
+```
+
+build
+
+```
+npm run build
+```
+
+dev
+
+```
+npm start
+```
+
+## Licence
+
+MIT
---
+++ webpack.common.js
@@ -0,0 +1,70 @@
+/* eslint-env node */
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(sc|c|sa|)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg)$/,
+        // 画像を埋め込まず任意のフォルダに保存する
+        loader: 'file-loader',
+        options: {
+          name: './images/[name].[ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    // 生成先のフォルダを空にする
+    new CleanWebpackPlugin(),
+    // cssファイルをjsファイルにバンドルせず処理するプラグイン
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new VueLoaderPlugin(),
+    // webpackで生成したjsとcssを読み込んだhtmlを作成
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+  ],
+};
