4d8a237e68c869414d61881f9365bd26d120ed77
tmiyachi <t.miyachi@hotmail.co.jp>
2021-04-18 10:51:16 +0900

    lintコマンドを追加，フォーマット修正

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,9 @@
 /* eslint-env node */
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
